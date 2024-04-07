f8c42fcd0930f5f17bf00df22245f50592c7859a
mursalatul <mursalatulislampallob@gmail.com>
2024-04-07 22:07:04 +0600

    link start button with progress bar

--- shutdowntimer_app.py
+++ shutdowntimer_app.py
@@ -1,14 +1,38 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import QtWidgets, QtCore
 from PyQt5.uic import loadUi
 import sys
 
-class MainUI(QMainWindow):
+class MainUI(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainUI, self).__init__()
         loadUi('shutdown_timer_gui.ui', self)
+        # find start button
+        self.start_bnt = self.findChild(QtWidgets.QPushButton, 'start_button')
+        self.start_bnt.clicked.connect(self.start_button_event)
+    
+    def start_button_event(self):
+        """
+        handle start button
+        """
+        print("start button pressed")
+        tm = 1
+        per_increase_delay = (tm * 60) / 100
+        self.start_progress_bar(10)
+
+    def start_progress_bar(self, per_increase_delay):
+        """
+        trigger the progress bar
+        """
+        # find progress barr
+        self.progress_bar = self.findChild(QtWidgets.QProgressBar, 'progressBar')
+        
+        # start the progressBar
+        while self.progress_bar.value() < 100:
+            QtCore.QTimer(self).start(per_increase_delay * 1000)
+            self.progress_bar.setValue(self.progress_bar.value() + 1)
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     ui = MainUI()
     ui.show()
     app.exec()
